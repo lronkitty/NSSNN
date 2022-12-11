16b1dded446e151458aee992f24ef8f85c2a293f
IronKitty <55337516+lronkitty@users.noreply.github.com>
2022-12-11 21:08:04 +0800

    Update eval_niid.sh

--- eval_niid.sh
+++ eval_niid.sh
@@ -2,14 +2,14 @@ conda activate torch_37
 python hsi_eval.py -a nssnn -ofn nssnn -s output \
 --gpu-ids 2  -ofd results/nssnn/icvl/niid/15/ \
 -r -rp checkpoints/icvl/niid.pth \
--tr testsets/ICVL/niid/15/ -gr testsets/ICVL/gt/
+-tr datasets/test/ICVL/niid/15/ -gr datasets/test/ICVL/gt/
 
 python hsi_eval.py -a nssnn -ofn nssnn -s output \
 --gpu-ids 2  -ofd results/nssnn/icvl/niid/55/ \
 -r -rp checkpoints/icvl/niid.pth \
--tr testsets/ICVL/niid/55/ -gr testsets/ICVL/gt/
+-tr datasets/test/ICVL/niid/55/ -gr datasets/test/ICVL/gt/
 
 python hsi_eval.py -a nssnn -ofn nssnn -s output \
 --gpu-ids 2  -ofd results/nssnn/icvl/niid/95/ \
 -r -rp checkpoints/icvl/niid.pth \
--tr testsets/ICVL/niid/95/ -gr testsets/ICVL/gt/
+-tr datasets/test/ICVL/niid/95/ -gr datasets/test/ICVL/gt/
